f0a68c69bdd8cf3997685dbc5ce074d9a10141a3
James Moy <jym2@njit.edu>
2019-03-04 10:14:09 -0500

    Initial Commit

---
+++ Hakuchou/Hakuchou.h
@@ -0,0 +1,19 @@
+//
+//  Hakuchou.h
+//  Hakuchou
+//
+//  Created by 香風智乃 on 3/4/19.
+//  Copyright © 2019 MAL Updater OS X Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Hakuchou.
+FOUNDATION_EXPORT double HakuchouVersionNumber;
+
+//! Project version string for Hakuchou.
+FOUNDATION_EXPORT const unsigned char HakuchouVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hakuchou/PublicHeader.h>
+
+
