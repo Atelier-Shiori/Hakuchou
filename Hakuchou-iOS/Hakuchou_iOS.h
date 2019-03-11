fec2fa6861b4839a9a55e6bdeb3f92097e5a591b
香風智乃 <koufuuchino@koufuuchinos-MacBook-puro.local>
2019-03-11 14:34:51 -0400

    Add includes

--- Hakuchou-iOS/Hakuchou_iOS.h
+++ Hakuchou-iOS/Hakuchou_iOS.h
@@ -15,5 +15,12 @@ FOUNDATION_EXPORT double Hakuchou_iOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char Hakuchou_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Hakuchou_iOS/PublicHeader.h>
-
+#import <Hakuchou_iOS/AtarashiiAPIListFormatKitsu.h>
+#import <Hakuchou_iOS/AtarashiiAPIListFormatAniList.h>
+#import <Hakuchou_iOS/AniListConstants.h>
+#import <Hakuchou_iOS/AniList.h>
+#import <Hakuchou_iOS/Kitsu.h>
+#import <Hakuchou_iOS/SharedHTTPManager.h>
+#import <Hakuchou_iOS/OAuthCredManager.h>
+#import <Hakuchou_iOS/AtarashiiDataObjects.h>
 
